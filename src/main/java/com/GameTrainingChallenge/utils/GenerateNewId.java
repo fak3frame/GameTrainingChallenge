0007dbf074e240822a9ae045931bacaf6546e0cb
Fak3Frame <kamil7745@gmail.com>
2019-01-27 17:02:38 +0100

    add static method returns highest number +1 of sent list or 0 if empty in GenerateNewId class

--- src/main/java/com/GameTrainingChallenge/utils/GenerateNewId.java
+++ src/main/java/com/GameTrainingChallenge/utils/GenerateNewId.java
@@ -1,4 +1,17 @@
 package com.GameTrainingChallenge.utils;
 
+import java.util.Set;
+
 public class GenerateNewId {
+    public static int createNewId(Set<Integer> ids){
+        if(ids.isEmpty()){
+            return 0;
+        }
+        else {
+            Integer id = ids.stream()
+                    .max(Integer::compare)
+                    .get();
+            return id+1;
+        }
+    }
 }
