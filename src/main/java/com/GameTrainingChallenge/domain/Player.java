dd4e577ea6c94c14a563b0feaf9f2e9bcc189f24
Fak3Frame <kamil7745@gmail.com>
2019-01-27 16:46:31 +0100

    add constructors in Player class

--- src/main/java/com/GameTrainingChallenge/domain/Player.java
+++ src/main/java/com/GameTrainingChallenge/domain/Player.java
@@ -13,4 +13,23 @@ public class Player {
     private String nickName;
 
     private TrainingExercise trainingExercise;
+
+
+    public Player(){
+        //for creating a new empty class object
+        //for hibernate
+    }
+
+    public Player(String playerName, int age){
+        //for creating Player from Starter class
+        this.playerName = playerName;
+        this.age = age;
+    }
+
+    public Player(Player player){
+        //for creating Player on web page by the user
+        this.playerName = player.playerName;
+        this.age = player.age;
+        this.trainingExercise = player.trainingExercise;
+    }
 }
