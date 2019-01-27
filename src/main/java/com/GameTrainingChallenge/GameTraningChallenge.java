972769657f2981b27ebd57b9600c137ff788e67c
Fak3Frame <kamil7745@gmail.com>
2019-01-27 16:37:25 +0100

    add main SpringBootApplication Class running context

---
+++ src/main/java/com/GameTrainingChallenge/GameTraningChallenge.java
@@ -0,0 +1,11 @@
+package com.GameTrainingChallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameTraningChallenge {
+    public static void main(String[] args) {
+        SpringApplication.run(GameTraningChallenge.class, args);
+    }
+}
