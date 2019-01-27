23b8aeae8aa4f4803ce0f75b7f859430dab3882c
Fak3Frame <kamil7745@gmail.com>
2019-01-27 16:54:13 +0100

    add primitive variables in TrainingExercise class

--- src/main/java/com/GameTrainingChallenge/domain/TrainingExercise.java
+++ src/main/java/com/GameTrainingChallenge/domain/TrainingExercise.java
@@ -1,4 +1,12 @@
 package com.GameTrainingChallenge.domain;
 
 public class TrainingExercise {
+
+    private int id;
+
+    private String nameOfExercise;
+
+    private int numberOfRepetitions;
+
+    private int rewardInPoints;
 }
