8c7edc728d4b120364471e065aaa0047532f8ce5
Fak3Frame <kamil7745@gmail.com>
2019-01-27 16:54:34 +0100

    add constructors in TrainingExercise class

--- src/main/java/com/GameTrainingChallenge/domain/TrainingExercise.java
+++ src/main/java/com/GameTrainingChallenge/domain/TrainingExercise.java
@@ -9,4 +9,18 @@ public class TrainingExercise {
     private int numberOfRepetitions;
 
     private int rewardInPoints;
+
+
+    public TrainingExercise(){
+        //for creating a new empty class object
+        //for hibernate
+    }
+
+    public TrainingExercise(String nameOfExercise, int numberOfRepetitions, int rewardInPoints){
+        //for creating Exercise from Starter class
+        //Exercise are creating ONLY in Starter class
+        this.nameOfExercise = nameOfExercise;
+        this.numberOfRepetitions = numberOfRepetitions;
+        this.rewardInPoints = rewardInPoints;
+    }
 }
