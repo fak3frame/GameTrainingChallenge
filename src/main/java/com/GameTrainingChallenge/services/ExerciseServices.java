d00793435cde86c1558db9a8dea9d30a6c998892
Fak3Frame <kamil7745@gmail.com>
2019-02-02 16:46:39 +0100

    add method sets random exercise to player using injected playerRepository object in ExerciseServices class, user select player and method sets random exercise from list of exercises

--- src/main/java/com/GameTrainingChallenge/services/ExerciseServices.java
+++ src/main/java/com/GameTrainingChallenge/services/ExerciseServices.java
@@ -1,10 +1,13 @@
 package com.GameTrainingChallenge.services;
 
+import com.GameTrainingChallenge.domain.TrainingExercise;
 import com.GameTrainingChallenge.domain.repository.ExerciseInterface;
 import com.GameTrainingChallenge.domain.repository.PlayerInterface;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 @Service
@@ -17,4 +20,21 @@ public class ExerciseServices {
     PlayerInterface playerRepository;
 
     final static Random rand = new Random();
+
+    public void assignRandomExercise(String nickName){
+        //method sets random exercise to selected player
+        //user select player on web page and controller sends nickname of this player
+        //method creates list of all exercise using method of exercise repository
+        // and select random exercise
+        //selected exercise is assign to player with sent nickname using player repository
+        // method which returns object of player by nickname and method sets exercise
+
+        List<TrainingExercise> exercises = new ArrayList<>(exerciseRepository.returnExercises());
+
+        int numberOfRandomExercise = rand.nextInt(exercises.size());
+
+        TrainingExercise trainingExercise = exercises.get(numberOfRandomExercise);
+
+        playerRepository.returnPlayerByName(nickName).ifPresent(x -> x.setTrainingExercise(trainingExercise));
+    }
 }
