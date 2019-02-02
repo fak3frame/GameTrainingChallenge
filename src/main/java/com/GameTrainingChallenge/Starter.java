254c4ac326f4bd30bcb7f2f1ef6926310fd395c1
Fak3Frame <kamil7745@gmail.com>
2019-02-02 17:42:22 +0100

    Add starter users, passwords and permissions

--- src/main/java/com/GameTrainingChallenge/Starter.java
+++ src/main/java/com/GameTrainingChallenge/Starter.java
@@ -1,9 +1,13 @@
 package com.GameTrainingChallenge;
 
+import com.GameTrainingChallenge.domain.LoginUsers;
 import com.GameTrainingChallenge.domain.Player;
 import com.GameTrainingChallenge.domain.TrainingExercise;
+import com.GameTrainingChallenge.domain.repository.CreateUserLoginInterace;
 import com.GameTrainingChallenge.domain.repository.ExerciseInterface;
 import com.GameTrainingChallenge.domain.repository.PlayerInterface;
+import com.GameTrainingChallenge.utils.Permissions;
+import com.GameTrainingChallenge.utils.PermissionsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Scope;
@@ -19,8 +23,38 @@ public class Starter implements CommandLineRunner {
     @Autowired
     PlayerInterface playerRepo;
 
+    @Autowired
+    CreateUserLoginInterace createUserLogin;
+
+    @Autowired
+    PermissionsRepository permissions;
+
     @Override
     public void run(String... args) throws Exception {
+
+
+        //----CREATING USERS AND PASSWORDS
+        LoginUsers user1 = new LoginUsers("user1", "pass1");
+        LoginUsers user2 = new LoginUsers("user2", "pass2");
+
+        //add them to database
+        createUserLogin.createUserLogin(user1);
+        createUserLogin.createUserLogin(user2);
+
+
+        //----CREATING PERMISSIONS
+        Permissions permissionsUser1 = new Permissions("user1", "USER");
+        Permissions permissionsUser2 = new Permissions("user2", "USER");
+        Permissions permissionsAdmin = new Permissions("user2", "ADMIN");
+
+        //add them to database
+        permissions.addPermissions(permissionsUser1);
+        permissions.addPermissions(permissionsUser2);
+        permissions.addPermissions(permissionsAdmin);
+
+
+
+
 
         //----CREATING STARTING PLAYERS (for tests)
         playerRepo.createPlayer("Kamil", 28);
