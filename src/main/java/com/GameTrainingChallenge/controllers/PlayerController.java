ed686f9683cc65f2999347bf5b1f28239ff34782
Fak3Frame <kamil7745@gmail.com>
2019-02-02 17:05:09 +0100

    add service injection (PlayerServices) in PlayerController class

--- src/main/java/com/GameTrainingChallenge/controllers/PlayerController.java
+++ src/main/java/com/GameTrainingChallenge/controllers/PlayerController.java
@@ -1,7 +1,12 @@
 package com.GameTrainingChallenge.controllers;
 
+import com.GameTrainingChallenge.services.PlayerServices;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class PlayerController {
+
+    @Autowired
+    PlayerServices playerServices;
 }
