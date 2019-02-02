f15dbc1ba69d3e418f310c0fc33501f43aab1108
Fak3Frame <kamil7745@gmail.com>
2019-02-02 16:57:40 +0100

    add new starting player and exercise and assign exercise to new player in starter class

--- src/main/java/com/GameTrainingChallenge/Starter.java
+++ src/main/java/com/GameTrainingChallenge/Starter.java
@@ -1,5 +1,7 @@
 package com.GameTrainingChallenge;
 
+import com.GameTrainingChallenge.domain.Player;
+import com.GameTrainingChallenge.domain.TrainingExercise;
 import com.GameTrainingChallenge.domain.repository.ExerciseInterface;
 import com.GameTrainingChallenge.domain.repository.PlayerInterface;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,5 +31,20 @@ public class Starter implements CommandLineRunner {
         exerciseRepo.createExercise("Push ups", 5, 10);
         exerciseRepo.createExercise("Squat Jumps", 10, 5);
 
+        //----CREATING STARTING PLAYER WITH ASSIGNED EXERCISE AT START
+        //creating next new player using special constructor in player class
+        Player player = new Player("Pawel", 29);
+
+        //creating new exercise
+        TrainingExercise exercise = new TrainingExercise("Situps", 10, 10);
+
+        //add new exercise to database
+        exerciseRepo.createExercise(exercise);
+
+        //assign exercise to player
+        player.setTrainingExercise(exercise);
+
+        //add new player with exercise to database
+        playerRepo.createPlayer(player);
     }
 }
