21575c3dd73bab00686778557f7beb45940044db
Fak3Frame <kamil7745@gmail.com>
2019-02-02 16:46:14 +0100

    add repository injection (ExerciseInterface and PlayerInterface) and Random variable in ExerciseServices class

--- src/main/java/com/GameTrainingChallenge/services/ExerciseServices.java
+++ src/main/java/com/GameTrainingChallenge/services/ExerciseServices.java
@@ -1,7 +1,20 @@
 package com.GameTrainingChallenge.services;
 
+import com.GameTrainingChallenge.domain.repository.ExerciseInterface;
+import com.GameTrainingChallenge.domain.repository.PlayerInterface;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Random;
 
 @Service
 public class ExerciseServices {
+
+    @Autowired
+    ExerciseInterface exerciseRepository;
+
+    @Autowired
+    PlayerInterface playerRepository;
+
+    final static Random rand = new Random();
 }
