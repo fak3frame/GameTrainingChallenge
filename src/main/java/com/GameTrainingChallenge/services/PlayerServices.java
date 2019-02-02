c118b815def0cbc98f975f82b3439cc0b57f9755
Fak3Frame <kamil7745@gmail.com>
2019-02-02 16:42:14 +0100

    add repository injection (PlayerInterface) in PlayerServices class

--- src/main/java/com/GameTrainingChallenge/services/PlayerServices.java
+++ src/main/java/com/GameTrainingChallenge/services/PlayerServices.java
@@ -1,7 +1,12 @@
 package com.GameTrainingChallenge.services;
 
+import com.GameTrainingChallenge.domain.repository.PlayerInterface;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PlayerServices {
+
+    @Autowired
+    PlayerInterface playerRepository;
 }
