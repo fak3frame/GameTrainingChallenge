27963e7bd103fd4c49ca44dd9257d75b2311f640
Fak3Frame <kamil7745@gmail.com>
2019-02-02 16:42:39 +0100

    add method returns list of all players using injected playerRepository object in PlayerServices class, method use method of repository class returns all player from database, method for return all players on main web page

--- src/main/java/com/GameTrainingChallenge/services/PlayerServices.java
+++ src/main/java/com/GameTrainingChallenge/services/PlayerServices.java
@@ -1,12 +1,24 @@
 package com.GameTrainingChallenge.services;
 
+import com.GameTrainingChallenge.domain.Player;
 import com.GameTrainingChallenge.domain.repository.PlayerInterface;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class PlayerServices {
 
     @Autowired
     PlayerInterface playerRepository;
+
+    public List<Player> returnPlayers(){
+        //method returns list of all players using injected playerRepository object
+        //method use method of repository class returns all players from database
+        //method for return all players on main web page
+
+        return new ArrayList<>(playerRepository.returnPlayers());
+    }
 }
