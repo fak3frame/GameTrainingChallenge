cffd4260e250d839d8287878e2c5c7df25c343ab
Fak3Frame <kamil7745@gmail.com>
2019-02-02 16:56:29 +0100

    add repository injection (ExerciseInterface and PlayerInterface) in starter class

--- src/main/java/com/GameTrainingChallenge/Starter.java
+++ src/main/java/com/GameTrainingChallenge/Starter.java
@@ -1,5 +1,8 @@
 package com.GameTrainingChallenge;
 
+import com.GameTrainingChallenge.domain.repository.ExerciseInterface;
+import com.GameTrainingChallenge.domain.repository.PlayerInterface;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
@@ -7,6 +10,12 @@ import org.springframework.stereotype.Component;
 @Component
 @Scope("singleton")
 public class Starter implements CommandLineRunner {
+
+    @Autowired
+    ExerciseInterface exerciseRepo;
+
+    @Autowired
+    PlayerInterface playerRepo;
 
     @Override
     public void run(String... args) throws Exception {
